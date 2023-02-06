6785e0f8c4b4e9fa75dda29fc8b3780aef1ea1e3
Zhanna Kulaginae <kuljeanne@gmail.com>
2023-02-06 11:33:31 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let config = Object.fromEntries(bracketsConfig);
+  let OpenBrackets = Object.keys(config);
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+
+    if (OpenBrackets.includes(str[i])) {
+       if(stack.length !== 0 && str[i] === stack[stack.length-1] && str[i] === config[ stack[stack.length - 1]]){
+        stack.pop(str[i])
+      }else{
+        stack.push(str[i])
+      }
+      // console.log(stack)
+    } else {
+      if (stack.length === 0) {
+        // console.log('false')
+        return false
+      } else if (stack.length !== 0) {
+        let topElement = stack[stack.length - 1];
+        // console.log('on the top is '+ topElement)
+        if (str[i] === config[topElement]) {
+          stack.pop(str[i])
+        } else {
+          return false
+        }
+
+      }
+    }
+  }
+  return (stack.length === 0)
 }
